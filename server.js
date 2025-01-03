39195d17004f5c5a704fb13bea5c649f85d3789b
pshakadaniel <pyaesonehein.71299@gmail.com>
2025-01-03 14:05:04 +0800

    updated cors origin

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 // Enable CORS for your frontend domain (update this to your actual frontend URL)
 app.use(
   cors({
-    origin: "https://harrypotterstudio.vercel.app", // Your actual frontend URL
+    origin:
+      "https://harrypotterstudio-h05zcljwb-pyae-sone-heins-projects.vercel.app", // Your actual frontend URL
     methods: ["GET", "POST"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type"], // Allowed headers
   })
